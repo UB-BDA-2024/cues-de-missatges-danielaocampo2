b0d73144310ee8b48f66032b7fb1e8f962b967ce
danielaocampo2 <31422275+danielaocampo2@users.noreply.github.com>
2024-05-22 01:30:54 +0200

    Update cassandra_client.py

--- shared/cassandra_client.py
+++ shared/cassandra_client.py
@@ -1,8 +1,9 @@
 from cassandra.cluster import Cluster
+from cassandra.policies import TokenAwarePolicy, DCAwareRoundRobinPolicy
 
 class CassandraClient:
     def __init__(self, hosts):
-        self.cluster = Cluster(hosts,protocol_version=4)
+        self.cluster = Cluster(hosts, load_balancing_policy=TokenAwarePolicy(DCAwareRoundRobinPolicy()),protocol_version=4)
         self.session = self.cluster.connect()
 
     def get_session(self):
@@ -10,3 +11,9 @@ class CassandraClient:
 
     def close(self):
         self.cluster.shutdown()
+
+    def execute(self, query, parameters=None):
+        if parameters is not None:
+            return self.get_session().execute(query, parameters)
+        else:
+            return self.get_session().execute(query)
